235ca052dea8729f4f59a4d42de6df13265049ff
Sarthak Raj <sarthakraj1256@gmail.com>
2026-09-11 01:09:27 +0530

    Delete logout.js

--- logout.js
+++
@@ -1,1 +0,0 @@
-export async function onRequestPost(){return new Response(JSON.stringify({ok:true}),{headers:{'Content-Type':'application/json','Set-Cookie':'admin_session=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0'}})}
